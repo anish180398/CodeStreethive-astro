52255a82e10b6d4856d8acd0d0aca51e1c240faa
Anish S <anish180398@gmail.com>
2025-01-24 03:30:27 +0530

    build ix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,15 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
-import vercel from "@astrojs/vercel/serverless";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://CodeStreetHive.com",
   image: {
-    domains: ["images.unsplash.com", "images.ctfassets.net"],
+    domains: ["images.unsplash.com","images.ctfassets.net"],
   },
   // i18n: {
   //   defaultLocale: "en",
